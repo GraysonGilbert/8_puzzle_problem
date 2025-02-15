bcfed746731749dc9de301b3b5a17efaa9806fe5
GraysonGilbert <graysongilbert@gmail.com>
2025-02-15 17:39:55 +0000

    saving progress. Can arrange board in proper 3x3 grid, locate 0 space, and determine valid moves

--- 8_puzzle_problem.py
+++ 8_puzzle_problem.py
@@ -1,7 +1,7 @@
 # Solving the 8 Puzzle Problem using Breadth First Search (BFS)
 
 import numpy as np
-
+from collections import deque
 
 
 """Step 2: Given the state of the current node, calculate the location of the blank tile [0] in
@@ -40,3 +40,72 @@ has been visited already and do not repeat that action for that node."""
 
                     The order of states should be from start node to goal node"""
 
+class PuzzleState:
+    def __init__(self, node_state_i, node_index_i, parent_node_index_i = None):
+        self.node_state_i = np.array(node_state_i)
+        self.node_index_i = node_index_i
+        self.parent_node_index_i = parent_node_index_i
+
+        self.board = self.arrange_board()
+    
+    def __eq__(self, other):
+        return np.array_equal(self.node_state_i, other.node_state_i)
+        
+
+    def __hash__(self):
+        return hash(tuple(self.node_state_i.flatten()))
+
+    def arrange_board(self): # Rearranges board state into 3x3 column-based grid
+        #data = self.node_state_i
+        return self.node_state_i.reshape((3,3), order="F")
+    
+
+    def find_zero(self): # Find and return 0 location in the current node state
+        row, col = np.where(self.board == 0)
+        
+        print(self.board)
+
+        return row[0], col[0]
+    
+    def get_possible_moves(self): # Find all valild moves given the current node state
+
+        row, col = self.find_zero()
+        #print(row, col)
+        moves = []
+
+        move_options = {
+            "up": (-1, 0),
+            "down": (1, 0),
+            "left": (0, -1),
+            "right": (0, 1)
+        }
+        
+        for move, (row_move, col_move) in move_options.items():
+            new_row, new_col = row + row_move, col + col_move
+
+            if 0 <= new_row < 3 and  0 <= new_col < 3:
+                moves.append(move)
+
+        print(moves)
+
+class PuzzleSolver:
+    def __init__():
+        pass
+    def action_move_up(self):
+        pass
+
+class PuzzleUtils:
+    def __init__(self):
+        pass
+
+
+#node_state = [1, 4, 7, 2, 5, 8, 3, 6, 0]
+node_state = [7, 6, 1, 5, 0, 4, 8, 3, 2]
+index = 0
+
+test = PuzzleState(node_state_i=node_state, node_index_i= index, parent_node_index_i=0)
+
+print(node_state)
+test.arrange_board()
+test.find_zero()
+test.get_possible_moves()
